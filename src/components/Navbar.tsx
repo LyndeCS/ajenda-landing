f7fcea8c252d6745982e4e4e512a65233175c43d
Christopher Lynde <lyndechris@gmail.com>
2023-08-10 13:57:41 -0400

    Navbar iteration 1
    -Navbar styled sufficiently for now
    -Extended tailwind theme to include mint/mintdarker color and Roboto font
    -Added Roboto font import to index

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,19 +4,26 @@ import React from 'react';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-white py-4">
-      <div className="container mx-auto px-4">
-        {/* Navbar content goes here */}
-        <div className="text-black">Ajenda</div>
-        <ul className="flex space-x-4">
-          <li className="text-black">Product</li>
-          <li className="text-black">Features</li>
-          <li className="text-black">Social</li>
-          <li className="text-black">Try Ajenda</li>
-        </ul>
+    <nav className="bg-white bg-opacity-90 backdrop-blur-md py-4">
+      <div className="mx-auto max-w-7xl px-4 flex justify-between items-center">
+        {/* Left-justified App Name */}
+        <div>
+          <span className="text-lg font-semibold text-black">Ajenda</span>
+        </div>
+
+        {/* Right-justified Links */}
+        <div className="space-x-4">
+          <a href="#" className="text-black hover:text-mint">Product</a>
+          <a href="#" className="text-black hover:text-mint">Features</a>
+          <a href="#" className="text-black hover:text-mint">Social</a>
+          <button className="rounded bg-mint text-white px-4 py-2 hover:bg-mintdarker">
+            Try Ajenda
+          </button>
+        </div>
       </div>
     </nav>
   );
 };
 
 export default Navbar;
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Roboto", "sans-serif"],
+			},
+			colors: {
+				mint: "#00BE92",
+				mintdarker: "#00a17b",
+			},
+		},
 	},
 	plugins: [],
 };
