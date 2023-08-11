b02585188bb8dd60b3b83e0f09e2e6fb9ec84e30
LyndeCS <lyndechris@gmail.com>
2023-08-11 17:40:04 -0400

    Reviews section
    -Created Reviews section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Usp from './components/Usp';
 import Features from './components/Features';
+import Reviews from './components/Reviews';
 
 const App: React.FC = () => {
   return (
@@ -14,6 +15,7 @@ const App: React.FC = () => {
       <Hero />
       <Usp />
       <Features />
+      <Reviews />
       {/* Other content of your landing page */}
     </div>
   );
