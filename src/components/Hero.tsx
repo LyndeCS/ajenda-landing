661fc830714263907b4be4d8aa894307529f68ca
Christopher Lynde <lyndechris@gmail.com>
2023-08-16 19:05:59 -0400

    Mobile Navbar/Hero updates
    -Added drop-down menu button to replace navbar links
    -Spacing tweaks to Hero section on mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,52 +4,51 @@ import heroImage from '../assets/hero.jpg'
 const Hero: React.FC = () => {
   return (
     <section className="bg-gradient-to-b from-teal-50 to-white z-10">
-<svg
+      <svg
+        className="absolute z-0"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 200 200"
+        fill="none"
+      >
+        {/* Teal Circle */}
+        <ellipse
+          cx="64"
+          cy="10"
+          rx="20"
+          ry="10"
+          z="-1"
+          fill="rgba(0, 190, 145, 0.5)"
+          filter="blur(10px)"
+        />
+        {/* Orange Circle */}
+        <circle
+          cx="40"
+          cy="10"
+          r="15"
+          z="-1"
+          fill="rgba(255, 103, 0, 0.3)"
+          filter="blur(10px)"
+        />
+      </svg>
 
-  className="absolute z-0"
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 200 200"
-  fill="none"
->
-  {/* Teal Circle */}
-  <ellipse
-    cx="64"
-    cy="10"
-    rx="20"
-    ry="10"
-    z="-1"
-    fill="rgba(0, 190, 145, 0.5)"
-    filter="blur(10px)"
-  />
-  {/* Orange Circle */}
-  <circle
-    cx="40"
-    cy="10"
-    r="15"
-    z="-1"
-    fill="rgba(255, 103, 0, 0.3)"
-    filter="blur(10px)"
-  />
-</svg>
-
-      <div className="mx-auto max-w-7xl px-4 flex flex-col items-center pt-28 z-10">
+      <div className="mx-auto max-w-7xl px-4 flex flex-col items-center z-10">
         
         {/* Header Text */}
-        <h1 className="text-5xl md:text-7xl pt-32 pb-8 font-extrabold text-black mb-4 text-center z-10">
+        <h1 className="text-4xl md:text-7xl pt-32 md:pt-60 pb-8 md:pb-12 font-extrabold text-black text-center z-10">
           Easy Schedule,
           <br />
           Easy Life
         </h1>
 
         {/* Description Text */}
-        <p className="text-xl md:text-2xl text-gray-600 text-center mb-20 z-10">
+        <p className="text-md md:text-2xl text-gray-600 text-center mb-10 md:mb-20 z-10">
           Simplify your life with Ajenda,
           <br />
           the key to stress-free task and schedule management
         </p>
 
         {/* Big Button */}
-        <a href="https://ajenda.web.app/" id="product-section" className="bg-gradient-to-br from-aqua via-mint to-mintlighter text-white px-12 py-4 rounded-lg font-medium text-2xl mb-20 shadow-lg border-[1px] border-mint hover:bg-gradient-to-br hover:from-mint hover:via-mintdarker hover:to-mintlighter hover:shadow-xl z-10">
+        <a href="https://ajenda.web.app/" id="product-section" className="bg-gradient-to-br from-aqua via-mint to-mintlighter text-white px-6 py-2 md:px-12 md:py-4 rounded-lg font-medium text-xl md:text-2xl mb-10 md:mb-20 shadow-lg border-[1px] border-mint hover:bg-gradient-to-br hover:from-mint hover:via-mintdarker hover:to-mintlighter hover:shadow-xl z-10">
             Try Ajenda
           </a>
 
@@ -57,7 +56,7 @@ const Hero: React.FC = () => {
         <img
           src={heroImage}
           alt="App Screenshot"
-          className="max-w-full rounded-lg shadow-fuller"
+          className="max-w-full rounded-lg shadow-full md:shadow-fuller"
         />
       </div>
     </section>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,76 @@
-// src/components/Navbar.tsx
-
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="absolute w-full z-50 bg-white bg-opacity-70 py-4 shadow-sm">
       <div className="mx-auto max-w-7xl px-4 flex justify-between items-center">
         {/* Left-justified App Name */}
         <div>
-          <a href="#" className="text-2xl font-medium text-black">Ajenda</a>
-        </div>
-
-        {/* Right-justified Links */}
-        <div className="space-x-8">
-          <a href="#product-section" className="text-black font-medium hover:text-mint">Product</a>
-          <a href="#features-section" className="text-black font-medium hover:text-mint">Features</a>
-          <a href="#reviews-section" className="text-black font-medium hover:text-mint">Reviews</a>
-          <a href="https://ajenda.web.app/" className="bg-gradient-to-br from-aqua via-mint to-mintlighter text-white font-medium px-4 py-2 border-[1px] border-mint hover:bg-gradient-to-br hover:from-mint hover:via-mintdarker hover:to-mintlighter hover:shadow rounded-lg">
-            Try Ajenda
+          <a href="#" className="text-2xl font-medium text-black">
+            Ajenda
           </a>
         </div>
+
+        {/* Desktop Links */}
+        <div className="hidden md:flex items-center space-x-8">
+          <a href="#product-section" className="text-black font-medium hover:text-mint">
+            Product
+          </a>
+          <a href="#features-section" className="text-black font-medium hover:text-mint">
+            Features
+          </a>
+          <a href="#reviews-section" className="text-black font-medium hover:text-mint">
+            Reviews
+          </a>
+          <a href="https://ajenda.web.app/" className="bg-gradient-to-br from-aqua via-mint to-mintlighter text-white font-medium px-4 py-2 border-[1px] border-mint hover:bg-gradient-to-br hover:from-mint hover:via-mintdarker hover:to-mintlighter hover:shadow rounded-lg">
+            Try Ajenda
+            </a>
+        </div>
+
+        {/* Mobile Menu Button */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-black">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        </div>
+
+        {/* Mobile Menu Dropdown */}
+        {isMenuOpen && (
+          <div className="absolute z-10 top-12 right-0 bg-white bg-opacity-100 p-2 shadow-md text-center">
+            <a href="#product-section" className="block text-black font-medium hover:text-mint mb-2">
+              Product
+            </a>
+            <a href="#features-section" className="block text-black font-medium hover:text-mint mb-2">
+              Features
+            </a>
+            <a href="#reviews-section" className="block text-black font-medium hover:text-mint mb-2">
+              Reviews
+            </a>
+            <a
+              href="https://ajenda.web.app/"
+              className="block bg-gradient-to-br from-aqua via-mint to-mintlighter text-white font-medium px-2 py-1 border-[1px] border-mint hover:bg-gradient-to-br hover:from-mint hover:via-mintdarker hover:to-mintlighter hover:shadow rounded-lg mb-2"
+            >
+              Try Ajenda
+            </a>
+          </div>
+        )}
       </div>
     </nav>
   );
 };
 
 export default Navbar;
-
