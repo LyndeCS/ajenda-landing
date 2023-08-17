93de3cf8fd24b6405f0eea0163a9c893430f17e9
Christopher Lynde <lyndechris@gmail.com>
2023-08-17 12:26:12 -0400

    Features section mobile update
    -Changed from 3col to 2col grid on mobile
    -Adjusted text size, spacing to fit better on mobile

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -5,40 +5,40 @@ import featuresPic3 from '../assets/features3.jpg';
 
 const Features: React.FC = () => {
   return (
-    <section id="features-section" className="py-16 bg-gradient-to-b from-white to-orange-50">
+    <section id="features-section" className="md:py-16 bg-gradient-to-b from-white to-orange-50">
       <div className="mx-auto max-w-7xl px-4">
-        <h2 className="text-4xl md:text-6xl font-extrabold mb-24 text-center">Stay organized across devices</h2>
-        <div className="bg-white border rounded-xl relative grid grid-cols-3">
-          <div className="md:relative">
+        <h2 className="text-4xl md:text-6xl font-extrabold mt-16 mb-10 md:mt-0 md:mb-24 text-center">Stay organized across devices</h2>
+        <div className="md:bg-white md:border rounded-xl relative grid grid-cols-2 md:grid-cols-3">
+          <div className="my-4 md:my-0 space-y-8 md:space-y-0 md:relative">
             <img
               src={featuresPic1}
               alt="Feature 1"
-              className="w-[9.75rem] h-[14rem] md:w-[19.5rem] md:h-[28rem] border-2 border-gray-600 overflow-hidden rounded-xl shadow-full transform -translate-x-8 -translate-y-8 absolute top-0 left-0"
+              className="w-[8rem] h-[12rem] md:w-[19.5rem] md:h-[28rem] border-2 border-gray-600 overflow-hidden rounded-xl shadow-full md:-translate-x-8 md:-translate-y-8 md:absolute md:top-0 md:left-0"
             />
             <img
               src={featuresPic2}
               alt="Feature 2"
-              className="w-[9.75rem] h-[14rem] md:w-[19.5rem] md:h-[28rem] border-2 border-gray-600 overflow-hidden rounded-xl shadow-full transform translate-x-8 translate-y-8 absolute top-0 left-0"
+              className="w-[8rem] h-[12rem] md:w-[19.5rem] md:h-[28rem] border-2 border-gray-600 overflow-hidden rounded-xl shadow-full md:translate-x-8 md:translate-y-8 md:absolute md:top-0 md:left-0"
             />
             <img
               src={featuresPic3}
               alt="Feature 3"
-              className="w-[9.75rem] h-[14rem] md:w-[19.5rem] md:h-[28rem] border-2 border-gray-600 overflow-hidden rounded-xl shadow-full transform translate-x-24 translate-y-24 absolute top-0 left-0"
+              className="w-[8rem] h-[12rem] md:w-[19.5rem] md:h-[28rem] border-2 border-gray-600 overflow-hidden rounded-xl shadow-full md:translate-x-24 md:translate-y-24 md:absolute md:top-0 md:left-0"
             />
           </div>
-          <div className="my-4 md:my-12 relative z-10 col-span-2 text-center">
-            <div className="space-y-24">
+          <div className="my-4 md:my-12 relative z-10 md:col-span-2 text-center">
+            <div className="space-y-16 md:space-y-24">
               <div>
-                <h3 className="text-3xl font-semibold pb-2">Flexibility, Unleashed</h3>
-                <p className="text-xl font-normal">Enjoy the full power of desktop features and the freedom of mobile convenience.</p>
+                <h3 className="text-2xl md:text-3xl font-semibold pb-2">Flexibility, Unleashed</h3>
+                <p className="text-md md:text-xl font-normal">Enjoy the full power of desktop features and the freedom of mobile convenience.</p>
               </div>
               <div>
-                <h3 className="text-3xl font-semibold pb-2">Touch & Go</h3>
-                <p className="text-xl font-normal">Effortlessly manage tasks with intuitive touch interactions, designed for mobile ease.</p>
+                <h3 className="text-2xl md:text-3xl font-semibold pb-2">Touch & Go</h3>
+                <p className="text-md md:text-xl font-normal">Effortlessly manage tasks with intuitive touch interactions, designed for mobile ease.</p>
               </div>
               <div>
-                <h3 className="text-3xl font-semibold pb-2">Stay Updated</h3>
-                <p className="text-xl font-normal">Sync tasks and updates in real time across all devices, keeping you in the loop.</p>
+                <h3 className="text-2xl md:text-3xl font-semibold pb-2">Stay Updated</h3>
+                <p className="text-md md:text-xl font-normal">Sync tasks and updates in real time across all devices, keeping you in the loop.</p>
               </div>
             </div>
           </div>
